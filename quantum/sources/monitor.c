dff942a45edd6a45650c707f4e4e4d656d7e9a56
UnixCodable <lucasbordanave@gmail.com>
2026-05-02 01:15:00 +0200

    feat: wip monitoring

--- quantum/sources/monitor.c
+++ quantum/sources/monitor.c
@@ -6,7 +6,7 @@
 /*   By: lbordana <lbordana@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/01 20:55:11 by lbordana          #+#    #+#             */
-/*   Updated: 2026/05/01 21:29:48 by lbordana         ###   ########.fr       */
+/*   Updated: 2026/05/02 00:42:50 by lbordana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ void	monitoring(struct s_codex_data *data)
 {
 	while (true)
 	{
-		break ;
+		if (data->number_of_compiles_required == 0)
+			break ;
 	}
 
 	return ;
