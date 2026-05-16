328feec0c8dcf7c09a15e994a31e0ed7d8ca955f
UnixCodable <lucasbordanave@gmail.com>
2026-05-17 01:51:49 +0200

    feat: wip monitoring

--- quantum/sources/utils/scheduling_functions.c
+++ quantum/sources/utils/scheduling_functions.c
@@ -6,13 +6,40 @@
 /*   By: lbordana <lbordana@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/16 19:13:07 by lbordana          #+#    #+#             */
-/*   Updated: 2026/05/17 00:30:40 by lbordana         ###   ########.fr       */
+/*   Updated: 2026/05/17 01:50:31 by lbordana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/codexion.h"
 
+void	s_swap(t_coders *heap)
+{
+	return ;
+}
+
+void	s_add(t_coders *heap)
+{
+	return ;
+}
+
+void	s_pop(t_coders *heap)
+{
+	return ;
+}
+
 bool	scheduler(t_coders *thread)
 {
+	if (strcmp(thread->data->scheduler, "fifo") == 0)
+	{
+		s_add(thread->dongle_left->priority_queue);
+		s_add(thread->dongle_left->priority_queue);
+		return (false);
+	}
+	if (strcmp(thread->data->scheduler, "edf") == 0)
+	{
+		s_add(thread->dongle_left->priority_queue);
+		s_add(thread->dongle_left->priority_queue);
+		return (false);
+	}
 	return (true);
 }
