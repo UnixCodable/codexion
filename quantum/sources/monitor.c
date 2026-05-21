f76689b709e4c44cc8bb03cf4aea86cb3b7ebc9b
UnixCodable <lucasbordanave@gmail.com>
2026-05-21 08:15:34 +0200

    feat: wip on mutexes datarace security

--- quantum/sources/monitor.c
+++ quantum/sources/monitor.c
@@ -6,7 +6,7 @@
 /*   By: lbordana <lbordana@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/01 20:55:11 by lbordana          #+#    #+#             */
-/*   Updated: 2026/05/21 01:34:04 by lbordana         ###   ########.fr       */
+/*   Updated: 2026/05/21 08:13:34 by lbordana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	*monitor_function(void *data_addr)
 	while (m_retrieve_running_state(data) == true)
 	{
 		pthread_mutex_lock(&data->enders_mutex);
-		if (data->ended_coders == data->number_of_coders - 1)
+		if (data->ended_coders >= data->number_of_coders - 1)
 			m_switch_running_state(data);
 		pthread_mutex_unlock(&data->enders_mutex);
 	}
